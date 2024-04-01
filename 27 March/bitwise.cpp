bb83fa2f7c739df6b753aa558609caab2b0067cb
shaninishad1999 <shaninishad1999@gmail.com>
2024-04-01 11:54:24 +0530

    27 march

---
+++ 27 March/bitwise.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+int a=10,b=12, r;
+r=a&b;
+cout<<"And =" <<r<<endl;
+ r=a|b;
+cout<<"or =" <<r<<endl;
+r=a^b;
+cout<<"not =" <<r<<endl;
+r=~a;
+cout<<"xor =" <<r<<endl;
+r=a<<2;
+cout<<"left shift =" <<r<<endl;
+r=a>>2;
+cout<<"right shift =" <<r<<endl;
+
+
+    
+}
