41fb6e3955683fa6c7e2b88108a7bc018bfb4289
shaninishad1999 <shaninishad1999@gmail.com>
2024-10-04 01:56:04 +0530

    bublesort

--- DSA/BubleSort/AcendingArray.cpp
+++ DSA/BubleSort/AcendingArray.cpp
@@ -5,26 +5,25 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {10, 20, 30, 60, 50, 10};
-    int length = sizeof(arr) / sizeof(arr[0]); // Calculate the length of the array
- cout << "Before Swapping" << endl;
+    // int arr[] = {10, 20, 30, 60, 50, 10};
+    int arr[] = {10, 50, 30, 100, 50, 60};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Swapping" << endl;
     for (int i = 0; i < length; i++)
     {
-        cout << arr[i] << " "; // Print each element
+        cout << arr[i] << " ";
     }
- cout << endl;
+    cout << endl;
     cout << "After Swapping" << endl;
     for (int i = 0; i < length; i++)
     {
-        for (int j = i + 1; j < length; j++)
-        {
-            if (arr[i] > arr[j])
+            if (arr[i] > arr[i+1])
             {
                 int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             }
-        }
+        
     }
     for (int i = 0; i < length; i++)
     {
